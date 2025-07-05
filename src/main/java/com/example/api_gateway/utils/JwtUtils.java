2097fe843ca6412966a312d4f1701aecac511e0c
NaGit09 <nhutanhtran806@gmail.com>
2025-07-05 07:29:10 +0700

    resolve cors error while client call api into gateway

--- src/main/java/com/example/api_gateway/utils/JwtUtils.java
+++ src/main/java/com/example/api_gateway/utils/JwtUtils.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class JwtUtils {
@@ -38,19 +36,6 @@ public class JwtUtils {
         Object roleObj = getClaims(token).get("roles");
         return (roleObj instanceof String) ? (String) roleObj : null;
     }
-    public List<String> getPermissionsFromToken(String token) {
-        Claims claims = getClaims(token);
-        Object raw = claims.get("permissions");
-
-        if (raw instanceof List<?> list) {
-            return list.stream()
-                    .map(Object::toString) // ép từng phần tử về String
-                    .collect(Collectors.toList());
-        }
-        return List.of(); // hoặc throw nếu bạn muốn bắt lỗi
-    }
-
-
     private Claims getClaims(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(getSigningKey())
